4ee38b381a157cf9980b33ffa958c7898d221a88
Lukáš Oplt <luk.oplt@gmail.com>
2026-05-24 07:32:29 +0200

    build: add SwiftSoup + ZIPFoundation for M3 ingestion

--- Package.swift
+++ Package.swift
@@ -18,13 +18,17 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0")
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.7.0"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19")
     ],
     targets: [
         .target(
             name: "AINotebookCore",
             dependencies: [
-                .product(name: "GRDB", package: "GRDB.swift")
+                .product(name: "GRDB", package: "GRDB.swift"),
+                "SwiftSoup",
+                .product(name: "ZIPFoundation", package: "ZIPFoundation")
             ]
         ),
         .executableTarget(
@@ -33,7 +37,8 @@ let package = Package(
         ),
         .testTarget(
             name: "AINotebookCoreTests",
-            dependencies: ["AINotebookCore"]
+            dependencies: ["AINotebookCore"],
+            resources: [.copy("Fixtures")]
         )
     ]
 )
